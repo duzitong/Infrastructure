195898fc80780518e7c4bb8020d88acca890c874
Zidong Lu <duzitong811@126.com>
2023-12-12 13:14:46 +0800

    Initial commit

---
+++ modules/base/main.tf
@@ -0,0 +1,52 @@
+locals {
+  serverNames = [for server in var.servers : server.name]
+}
+
+module "hci" {
+  source                = "../hci"
+  location              = var.location
+  siteId                = var.siteId
+  domainFqdn            = var.domainFqdn
+  startingAddress       = var.startingAddress
+  endingAddress         = var.endingAddress
+  defaultGateway        = var.defaultGateway
+  dnsServers            = var.dnsServers
+  adouPath              = var.adouPath
+  tenant                = var.tenant
+  domainServerIP        = var.domainServerIP
+  servers               = var.servers
+  managementAdapters    = var.managementAdapters
+  storageNetworks       = var.storageNetworks
+  subId                 = var.subId
+  domainAdminUser       = var.domainAdminUser
+  domainAdminPassword   = var.domainAdminPassword
+  localAdminUser        = var.localAdminUser
+  localAdminPassword    = var.localAdminPassword
+  servicePrincipalId     = var.servicePrincipalId
+  servicePrincipalSecret = var.servicePrincipalSecret
+  destory_adou          = var.destory_adou
+  virtualHostIp         = var.virtualHostIp
+  dcPort                = var.dcPort
+  serverPorts           = var.serverPorts
+}
+
+module "extension" {
+  source         = "../hci-extensions"
+  depends_on     = [module.hci]
+  resourceGroup  = module.hci.resourceGroup
+  siteId         = var.siteId
+  clusterId      = module.hci.cluster.id
+  serverNames    = local.serverNames
+  enableInsights = var.enableInsights
+  enableAlerts   = var.enableAlerts
+}
+
+module "vm" {
+  count            = var.enableVM ? 1 : 0
+  source           = "../hci-vm"
+  depends_on       = [module.hci]
+  customLocationId = module.hci.customlocation.id
+  resourceGroupId  = module.hci.resourceGroup.id
+  userStorageId    = module.hci.userStorage1.id
+  location         = module.hci.resourceGroup.location
+}
---
+++ modules/hci/predeploy.tf
@@ -0,0 +1,116 @@
+//Authorize RP
+locals {
+  SPRoleList = [
+    # "Azure Connected Machine Resource Manager",
+    "User Access Administrator",
+    "contributor"
+  ]
+}
+
+resource "azurerm_role_assignment" "ServicePrincipalRoleAssign" {
+  for_each             = toset(local.SPRoleList)
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = each.value
+  principal_id         = var.rp_principal_id
+}
+
+
+
+data "azurerm_client_config" "current" {}
+resource "random_id" "twobyte" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 2
+}
+
+resource "azurerm_key_vault" "DeploymentKeyVault" {
+  name                = "${var.siteId}-kv-${random_id.twobyte.hex}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  depends_on          = [terraform_data.ad_creation_provisioner]
+
+  enabled_for_deployment          = true
+  enabled_for_template_deployment = true
+  enabled_for_disk_encryption     = true
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  // arm template has enableSoftDelete": false, but terraform can't disable it after version 2.42.
+  soft_delete_retention_days    = 30
+  enable_rbac_authorization     = true
+  public_network_access_enabled = true
+  sku_name                      = "standard"
+
+}
+
+// TODO: Add RBAC to keyvault
+
+resource "azurerm_key_vault_secret" "AzureStackLCMUserCredential" {
+  name         = "AzureStackLCMUserCredential"
+  content_type = "Secret"
+  value        = base64encode("${var.domainAdminUser}:${var.domainAdminPassword}")
+  key_vault_id = azurerm_key_vault.DeploymentKeyVault.id
+  depends_on   = [azurerm_key_vault.DeploymentKeyVault]
+}
+
+resource "azurerm_key_vault_secret" "LocalAdminCredential" {
+  name         = "LocalAdminCredential"
+  content_type = "Secret"
+  value        = base64encode("${var.localAdminUser}:${var.localAdminPassword}")
+  key_vault_id = azurerm_key_vault.DeploymentKeyVault.id
+  depends_on   = [azurerm_key_vault.DeploymentKeyVault]
+}
+
+resource "azurerm_key_vault_secret" "arbDeploymentSpnName" {
+  name         = "DefaultARBApplication"
+  content_type = "Secret"
+  value        = base64encode("${var.servicePrincipalId}:${var.servicePrincipalSecret}")
+  key_vault_id = azurerm_key_vault.DeploymentKeyVault.id
+  depends_on   = [azurerm_key_vault.DeploymentKeyVault]
+}
+
+resource "azurerm_key_vault_secret" "storageWitnessName" {
+  name         = "WitnessStorageKey"
+  content_type = "Secret"
+  value        = base64encode(azurerm_storage_account.witness.primary_access_key)
+  key_vault_id = azurerm_key_vault.DeploymentKeyVault.id
+  depends_on   = [azurerm_key_vault.DeploymentKeyVault]
+}
+
+//6. Get Arc server & assign roles to its system identity
+# Get resources by type
+
+//setup WSManCredSSP
+locals {
+  iplist = join(",", [for server in var.servers : server.ipv4Address])
+}
+resource "terraform_data" "WSManSetting" {
+  depends_on = [terraform_data.ad_creation_provisioner]
+  count      = var.virtualHostIp == "" ? 1 : 0
+  provisioner "local-exec" {
+    command     = "Enable-WSManCredSSP -Role Client -DelegateComputer ${local.iplist} -Force -ErrorAction SilentlyContinue"
+    interpreter = ["PowerShell"]
+  }
+}
+
+module "servers" {
+  for_each = {
+    for index, server in var.servers :
+    server.name => server.ipv4Address
+  }
+  depends_on            = [azurerm_resource_group.rg, terraform_data.ad_creation_provisioner, terraform_data.WSManSetting]
+  source                = "./hciserver"
+  resourceGroup         = azurerm_resource_group.rg.name
+  serverName            = each.key
+  localAdminUser        = var.localAdminUser
+  localAdminPassword    = var.localAdminPassword
+  serverIP              = var.virtualHostIp == "" ? each.value : var.virtualHostIp
+  winrmPort             = var.virtualHostIp == "" ? 5985 : var.serverPorts[each.key]
+  subId                 = var.subId
+  location              = var.location
+  tenant                = var.tenant
+  servicePrincipalId     = var.servicePrincipalId
+  servicePrincipalSecret = var.servicePrincipalSecret
+  expandC               = var.virtualHostIp == "" ? false : true
+}
